ffa99e2b81e90cb9e831d2dab22e8267cd33acb8
jason-heroes620 <jason.w@heroes.my>
2026-01-10 01:43:51 +0800

    update cors config to include art-fair.heroes.my

--- config/cors.php
+++ config/cors.php
@@ -22,7 +22,8 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'http://192.168.100.45:5173'
+        'http://192.168.100.45:5173',
+        'https://art-fair.heroes.my'
     ],
 
     'allowed_origins_patterns' => [],
